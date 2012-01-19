6921811b15f531862cc2b6a75ecc107fdfe0a5d0
Peter Sanford <psanford@nearbuysystems.com>
2012-01-19 15:44:49 -0800

    Don't redirect to the login page after an invite.
    We want to go back to the user list.

--- app/controllers/devise/invitations_controller.rb
+++ app/controllers/devise/invitations_controller.rb
@@ -18,7 +18,7 @@ class Devise::InvitationsController < ApplicationController
 
     if resource.errors.empty?
       set_flash_message :notice, :send_instructions, :email => self.resource.email
-      respond_with resource, :location => after_invite_path_for(resource)
+      respond_with resource, :location => after_update_path_for(resource)
     else
       respond_with_navigational(resource) { render_with_scope :new }
     end
